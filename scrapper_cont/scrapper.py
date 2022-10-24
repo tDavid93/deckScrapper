6c8606c519feed0bf9ea31a7b35ae7bd8c66b0fa
bonefire <toth.david.938@gmail.com>
2022-10-25 00:49:36 +0200

    .

--- scrapper_cont/scrapper.py
+++ scrapper_cont/scrapper.py
@@ -63,7 +63,7 @@ class scrapper():
           resp = sqlCursor.fetchall()
           
           if resp == []:
-              sqlCursor.execute("INSERT INTO deck_names  (deck_id, name, fetched, id_fetch) values (%s,%s,%s,%s);", (0,link,0,0))
+              sqlCursor.execute("INSERT INTO deck_names  (deck_id, name, fetched) values (%s,%s,%s);", (0,link,0))
               sqlConnector.commit()
       sqlCursor.close()
 
@@ -194,7 +194,7 @@ class scrapper():
         return
     with Session(this.engineP) as session:
     
-        session.execute("update decks Set fetched = 1 where url={0}".format(link[0]))
+        session.execute("update decks Set fetched = 1, id_fetch=0 where url={0}".format(link[0]))
         session.commit()
 
 

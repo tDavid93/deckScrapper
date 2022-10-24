385aaf7338485624dac57a3a2f1a9450e5577af5
bonefire <toth.david.938@gmail.com>
2022-10-25 01:14:11 +0200

    .

--- scrapper_cont/idFetcher.py
+++ scrapper_cont/idFetcher.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 import scrapper as sc
 from sqlalchemy.orm import Session
+import sqlalchemy
 
 connectionString = 'postgresql://postgres:mtgai@host.docker.internal/decks'
 engineP = create_engine(connectionString)
@@ -21,17 +22,20 @@ while(True):
 
   
   for table in tableNameDf.to_numpy():
-    #print("idFetcher: {0}/{1}::{2}".format(counter,all_t,table[0]))
-    counter = counter +1
-    deckTemp = pd.read_sql(""" select * from "decks"."deck_{0}";""".format(table[0]), engineP)
-    #print(deckTemp["Name"].apply(lambda x : nameIddf.loc[nameIddf["name"] == difflib.get_close_matches(x, nameIddf["name"])[0]]).shape)
-    deckTemp["OracleId"] = deckTemp["Name"].apply(sc.scrapper.resolve_name,args=(nameIddf,))
+    try:    #print("idFetcher: {0}/{1}::{2}".format(counter,all_t,table[0]))
+      counter = counter +1
+      deckTemp = pd.read_sql(""" select * from "decks"."deck_{0}";""".format(table[0]), engineP)
+      #print(deckTemp["Name"].apply(lambda x : nameIddf.loc[nameIddf["name"] == difflib.get_close_matches(x, nameIddf["name"])[0]]).shape)
+      deckTemp["OracleId"] = deckTemp["Name"].apply(sc.scrapper.resolve_name,args=(nameIddf,))
+      
     
-  
+      
+      deckTemp.to_sql("deck_{0}".format(table[0]),engineP,schema="deck_f",if_exists='replace')
+      
+      with Session(engineP) as session:
     
-    deckTemp.to_sql("deck_{0}".format(table[0]),engineP,schema="deck_f",if_exists='replace')
-    
-    with Session(engineP) as session:
-  
-      session.execute("update decks Set id_fetch = 1 where id={0}".format(table[0]))
-      session.commit()
+        session.execute("update decks Set id_fetch = 1 where id={0}".format(table[0]))
+        session.commit()
+    except (sqlalchemy.exc.ProgrammingError, sqlalchemy.exc.OperationalError) as e:
+      print(e)
+      
